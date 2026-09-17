b8fd60f452098f323a52ec0ed5c8e428760e1d4b
bersavesab-hue <bersavesab@gmail.com>
2026-09-17 18:45:58 +0800

    feat(ui): expose ui foundation

---
+++ src/ui/index.js
@@ -0,0 +1,9 @@
+export { UI_TOKENS } from "./theme/tokens.js";
+export { buildGameClockModel, toCalendarDate } from "./components/GameClockModel.js";
+export { buildGlobalTopBarModel, buildNoticeTickerModel } from "./components/GlobalChromeModel.js";
+export { PageRegistry, pageRegistry } from "./registry/PageRegistry.js";
+export { CORE_PAGES, registerDefaultPages } from "./registry/defaultPages.js";
+export {
+  renovationMobilePageSystem,
+  RenovationMobilePageSystem
+} from "./renovation/index.js";
