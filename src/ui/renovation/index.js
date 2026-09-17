13be4b4032d411417c26c1f25595e53fcffd7827
bersavesab-hue <bersavesab@gmail.com>
2026-09-17 19:45:16 +0800

    Export renovation workspace helpers

--- src/ui/renovation/index.js
+++ src/ui/renovation/index.js
@@ -9,3 +9,16 @@ export {
   gridPointFromClient,
   placementStyle
 } from "./RenovationMobileView.js";
+
+export {
+  WORKSPACE_MODE,
+  RENOVATION_WORKSPACE_MODE_LABELS,
+  getFloorArea,
+  getWorkspaceMode,
+  getZoomConfig,
+  normalizeZoom,
+  buildWorkspaceZones,
+  getViewBounds,
+  placementIntersectsBounds,
+  buildMinimapModel
+} from "./RenovationWorkspaceModel.js";
