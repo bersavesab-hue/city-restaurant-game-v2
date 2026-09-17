8a973e92d650d6f81be32f4c9a040b43cea9759b
bersavesab-hue <bersavesab@gmail.com>
2026-09-17 18:54:03 +0800

    Export restaurant home UI

---
+++ src/ui/pages/restaurant/index.js
@@ -0,0 +1,9 @@
+export {
+  restaurantHomePageSystem,
+  RestaurantHomePageSystem
+} from "./RestaurantHomePageSystem.js";
+
+export {
+  restaurantHomeView,
+  RestaurantHomeView
+} from "./RestaurantHomeView.js";
