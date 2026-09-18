75946c2b259adaa24c92d2fcd649630004e928e0
bersavesab-hue <bersavesab@gmail.com>
2026-09-18 20:30:44 +0800

    customers: bootstrap 28 formal segments

--- src/systems/CustomerSegmentBootstrapSystem.js
+++ src/systems/CustomerSegmentBootstrapSystem.js
@@ -1,12 +1,12 @@
 import { customerSegmentSystem } from "./CustomerSegmentSystem.js";
-import { CUSTOMER_SEGMENTS_V2 } from "../data/customerSegments.v2.js";
+import { CUSTOMER_SEGMENTS_V3 } from "../data/customerSegments.v3.js";
 
 class CustomerSegmentBootstrapSystem {
   ensureLoaded({ overwrite = false } = {}) {
     const existing = customerSegmentSystem.getAll();
 
     if (existing.length === 0 || overwrite) {
-      customerSegmentSystem.load(CUSTOMER_SEGMENTS_V2, { overwrite: true });
+      customerSegmentSystem.load(CUSTOMER_SEGMENTS_V3, { overwrite: true });
     }
 
     return customerSegmentSystem.getAll();
