2d9dfb163270b3149cffa5e53996f5b219385ff5
bersavesab-hue <bersavesab@gmail.com>
2026-09-19 12:09:38 +0800

    save: define current save and state schema versions

---
+++ src/core/SaveSchema.js
@@ -0,0 +1,11 @@
+export const CURRENT_STATE_SCHEMA_VERSION =
+  2;
+
+export const CURRENT_SAVE_FORMAT_VERSION =
+  2;
+
+export const SUPPORTED_SAVE_FORMAT_VERSIONS =
+  Object.freeze([
+    1,
+    2
+  ]);
