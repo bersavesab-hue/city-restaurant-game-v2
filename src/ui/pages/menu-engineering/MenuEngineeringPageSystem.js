3647ea4694f6a213df9a0b2e6fa963d3d37a24c9
bersavesab-hue <bersavesab@gmail.com>
2026-09-19 14:54:07 +0800

    fix: remove duplicated formal page declarations

--- src/ui/pages/menu-engineering/MenuEngineeringPageSystem.js
+++ src/ui/pages/menu-engineering/MenuEngineeringPageSystem.js
@@ -6,6 +6,7 @@ import {
   buildFormalPageChrome
 } from "../../components/FormalPageChromeModel.js";
 
+
 class MenuEngineeringPageSystem {
   getPage(
     restaurantId
@@ -51,52 +52,11 @@ class MenuEngineeringPageSystem {
         }
       );
 
-    const analysis =
-      menuEngineeringSystem
-        .analyze(
-          restaurantId
-        );
-
-    const notices =
-      [];
-
-    if (
-      analysis.counts.puzzle >
-        0 ||
-      analysis.counts.dog >
-        0
-    ) {
-      notices.push({
-        id:
-          "menu_engineering_attention",
-        type:
-          "warning",
-        title:
-          "菜单诊断",
-        message:
-          `当前有${analysis.counts.puzzle}道问题菜、${analysis.counts.dog}道低效菜需要关注`,
-        priority:
-          70
-      });
-    }
-
-    const chrome =
-      buildFormalPageChrome(
-        restaurantId,
-        {
-          notices
-        }
-      );
-
     return {
       pageId:
         "menu-engineering",
 
-      topBar:
-        chrome.topBar,
-
-      noticeTicker:
-        chrome.noticeTicker,
+      restaurantId,
 
       topBar:
         chrome.topBar,
@@ -112,8 +72,10 @@ class MenuEngineeringPageSystem {
   }
 }
 
+
 export const menuEngineeringPageSystem =
   new MenuEngineeringPageSystem();
+
 
 export {
   MenuEngineeringPageSystem
