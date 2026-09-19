cfc43bc50b4d5094c2f6dc9b2cace63ccf200865
bersavesab-hue <bersavesab@gmail.com>
2026-09-19 14:34:31 +0800

    release: publish 0.8.67 journey-fix metadata

--- src/release/ReleaseInfo.js
+++ src/release/ReleaseInfo.js
@@ -7,10 +7,10 @@ export const RELEASE_INFO =
       "com.cityrestaurant.v2playtest",
 
     versionName:
-      "0.8.66",
+      "0.8.67",
 
     versionCode:
-      866,
+      867,
 
     channel:
       "playtest",
