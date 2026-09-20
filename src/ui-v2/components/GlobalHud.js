299fdd864feb71de1c09eebd16125e73a9a33c82
bersavesab-hue <bersavesab@gmail.com>
2026-09-21 03:56:10 +0800

    refactor(ui-v2): lock hud internal component boxes

--- src/ui-v2/components/GlobalHud.js
+++ src/ui-v2/components/GlobalHud.js
@@ -8,29 +8,31 @@ function renderGlobalHud() {
           '<small>管理旗下 3 家门店</small>' +
         '</div>' +
       '</section>' +
+
       '<section class="ui-v2-hud__simulation" data-ui-box="hud-simulation">' +
-        '<div class="ui-v2-box ui-v2-box--hud-icon" data-ui-box="hud-weather"></div>' +
+        '<div class="ui-v2-box ui-v2-box--hud-weather" data-ui-box="hud-weather"></div>' +
         '<div class="ui-v2-hud__clock">' +
           '<small>第1年 4月10日 周三</small>' +
           '<strong class="ui-v2-tabular">11:30</strong>' +
         '</div>' +
-        '<div class="ui-v2-hud__speed-row">' +
-          '<button class="ui-v2-box ui-v2-box--hud-action" type="button">Ⅱ</button>' +
-          '<button class="ui-v2-box ui-v2-box--hud-action is-active" type="button">1x</button>' +
-          '<button class="ui-v2-box ui-v2-box--hud-action" type="button">2x</button>' +
-          '<button class="ui-v2-box ui-v2-box--hud-action" type="button">4x</button>' +
+        '<div class="ui-v2-hud__speed-row" data-ui-box="hud-speed-row">' +
+          '<button class="ui-v2-box ui-v2-box--hud-speed" type="button">Ⅱ</button>' +
+          '<button class="ui-v2-box ui-v2-box--hud-speed is-active" type="button">1x</button>' +
+          '<button class="ui-v2-box ui-v2-box--hud-speed" type="button">2x</button>' +
+          '<button class="ui-v2-box ui-v2-box--hud-speed" type="button">4x</button>' +
         '</div>' +
       '</section>' +
+
       '<section class="ui-v2-hud__resources" data-ui-box="hud-resources">' +
-        '<div class="ui-v2-hud__resource-row">' +
-          '<span class="ui-v2-box ui-v2-box--hud-icon"></span>' +
+        '<div class="ui-v2-hud__resource-row ui-v2-hud__resource-row--money">' +
+          '<span class="ui-v2-box ui-v2-box--hud-icon" data-ui-box="hud-money-icon"></span>' +
           '<strong class="ui-v2-tabular">¥52,800</strong>' +
-          '<span class="ui-v2-box ui-v2-box--hud-action"></span>' +
+          '<span class="ui-v2-box ui-v2-box--hud-resource-action" data-ui-box="hud-add"></span>' +
         '</div>' +
-        '<div class="ui-v2-hud__resource-row">' +
-          '<span class="ui-v2-box ui-v2-box--hud-icon"></span>' +
+        '<div class="ui-v2-hud__resource-row ui-v2-hud__resource-row--level">' +
+          '<span class="ui-v2-box ui-v2-box--hud-icon" data-ui-box="hud-level-icon"></span>' +
           '<strong>Lv.3</strong>' +
-          '<span class="ui-v2-box ui-v2-box--hud-icon"></span>' +
+          '<span class="ui-v2-box ui-v2-box--hud-icon" data-ui-box="hud-rating-icon"></span>' +
           '<strong class="ui-v2-tabular">4.8</strong>' +
         '</div>' +
       '</section>' +
