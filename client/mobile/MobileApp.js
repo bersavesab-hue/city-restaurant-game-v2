b804f7f8209b7783e4d23b5471ec4ddcf9523b02
bersavesab-hue <bersavesab@gmail.com>
2026-09-25 01:34:02 +0800

    chore(ui): build complete live homepage overlay

--- client/mobile/MobileApp.js
+++ client/mobile/MobileApp.js
@@ -186,7 +186,7 @@ function renderStore() {
     <section
       class="home-master-page"
       data-ui-component="home-master"
-      data-home-master-version="6"
+      data-home-master-version="7"
       aria-label="门店首页静态母版"
     >
       <picture class="home-master-picture" aria-hidden="true">
