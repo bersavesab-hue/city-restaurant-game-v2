e6118cb93514a1a7042278a6feeb41ef575f085f
bersavesab-hue <bersavesab@gmail.com>
2026-09-25 16:36:53 +0800

    chore(ui): build corrected hard-mask homepage

--- client/mobile/MobileApp.js
+++ client/mobile/MobileApp.js
@@ -186,7 +186,7 @@ function renderStore() {
     <section
       class="home-master-page"
       data-ui-component="home-master"
-      data-home-master-version="9"
+      data-home-master-version="10"
       aria-label="门店首页静态母版"
     >
       <picture class="home-master-picture" aria-hidden="true">
